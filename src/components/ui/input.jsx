d11bc771bbfba80ab94c0f30bd0a7eedd6d0115e
unknown <marcio.silva@linkcom.com.br>
2022-02-10 15:02:13 -0300

    armazenando valores na tela

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,9 +1,17 @@
 export const Input= (props)=>{
 
+const handleChange = (event) =>{
+    const valor = event.target.value
+    console.log('mudou', valor)
+    props.setValue(valor)
+}
+
 return(
     <div className = "form-group">
        <label className="from-label" htmlFor={props.id}>{props.label} </label>
-       <input  type = {props.type} id= {props.id}  name = {props.name} placeholder = {props.placeholder}/>
+       <input  type = {props.type} 
+       onChange ={handleChange}
+       id= {props.id}  name = {props.name} placeholder = {props.placeholder}/>
     </div>
 )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
 import { Botao } from "../components/ui/botao"
 import { Input } from "../components/ui/input"
 
-const Login = () => {
+const Login = () => { 
+
+    const [PARAM_1, PARAM_2] = useState('VALOR INICIAL')
+    // PARAM 1 - VALOR DA MINHA VARIÁVEL
+    // PARAM 2 - FUNÇÃO QUE ALTERA O PARAM_1
+
+    const [email,setEmail] = useState()
+    const [password,setPassword] = useState()
+
+    //console.log('email',email);
+    //console.log('senha',senha);
+
+
+
     return (
         <div>
             <h1>Login</h1>
             <div className = "form">
-                <Input label ="email" type = "text" id="email" name = "email" placeholder="seu email"/>
-                <Input label ="senha" type = "password" id="password" name = "password" placeholder="sua senha"/>
+                <Input setValue={setEmail} label ="email" type = "text" id="email" name = "email" placeholder="seu email"/>
+                <Input setValue={setPassword} label ="senha" type = "password" id="password" name = "password" placeholder="sua senha"/>
                 <Botao>ACESSAR</Botao>
             </div>
         </div>
